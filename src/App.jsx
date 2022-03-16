57fd21ee4164fe57b356492851deddcf3815c20e
Prokash Pul <pprokash102@gmail.com>
2022-03-16 23:45:46 +0600

    mealdb react app project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <h2>Welcome to MealDB</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
