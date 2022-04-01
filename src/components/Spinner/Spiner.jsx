0b9cece371025463644224795d9f244dbbedf326
prokash <pprokash102@gmail.com>
2022-04-01 17:03:49 +0600

    update

--- src/components/Spinner/Spiner.jsx
+++ src/components/Spinner/Spiner.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import { ClipLoader } from "react-spinners";
 import "./Spinner.css";
-const Spiner = () => {
-  let [loading, setLoading] = useState(true);
+const Spiner = ({ loading }) => {
   return (
     <div className="spinner">
       <ClipLoader color={"#9013FE"} loading={loading} size={150} />
